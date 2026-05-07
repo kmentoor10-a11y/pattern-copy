0e87352e46cb64581983f03719510d59e16ef1ea
kmentoor10-a11y <kmentoor10@gmail.com>
2026-05-07 22:39:01 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,545 @@
+// Load settings from session storage
+
+const settings = JSON.parse(sessionStorage.getItem("settings"));
+
+// if no settings
+if (!settings) {
+  alert("No settings found. Go back to launcher.");
+  window.close();
+}
+
+//player object
+
+const player = {
+  name: settings.playerName,
+  bestScore: 0,
+};
+
+const savedBest = getCookie("bestScore");
+if (savedBest) {
+  player.bestScore = parseInt(savedBest);
+}
+
+function getPreviewTime() {
+  if (settings.difficulty === "easy") return 2500;
+  if (settings.difficulty === "medium") return 1500;
+  return 800; //hard
+}
+
+//game state object
+const gameState = {
+  round: 0,
+  score: 0,
+  lives: 3,
+  pattern: [],
+  userPattern: [],
+  gridSize: settings.gridSize,
+  previewTime: getPreviewTime("settings.difficulty"),
+};
+
+//DOM Elements
+const displayPlayer = document.getElementById("displayPlayer");
+const displayRound = document.getElementById("displayRound");
+const displayScore = document.getElementById("displayScore");
+const displayLives = document.getElementById("displayLives");
+const displayPreview = document.getElementById("displayPreview");
+const displayBestScore = document.getElementById("displayBestScore");
+
+const loadBtn = document.getElementById("loadBtn");
+const messageArea = document.getElementById("messageArea");
+const timerBar = document.getElementById("timerBar");
+const gridArea = document.getElementById("gridArea");
+
+//Display Initial Information
+function updateDisplay() {
+  displayPlayer.textContent = player.name;
+  displayRound.textContent = gameState.round;
+  displayScore.textContent = gameState.score;
+  displayLives.textContent = gameState.lives;
+  displayBestScore.textContent = player.bestScore;
+  displayPreview.textContent = gameState.previewTime + " ms";
+}
+
+updateDisplay();
+
+//Load Best Score from cookie
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  for (let c of cookies) {
+    const [key, value] = c.split("=");
+    if (key === name) return value;
+  }
+  return null;
+}
+
+//Symbol
+function getRandomSymbol() {
+  const symbols = ["★", "●", "▲", "■", "◆", "✦", "✿", "✖"];
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+//Creating the Grid
+function createGrid() {
+  gridArea.innerHTML = "";
+
+  const size = gameState.gridSize;
+  gridArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("pattern-cell");
+    cell.dataset.index = i;
+
+    //Apply themes
+
+    //Colours
+    cell.addEventListener("click", handleCellClick);
+    gridArea.appendChild(cell);
+
+    //Symbols
+    if (settings.theme === "symbols") {
+      cell.textContent = getRandomSymbol();
+      cell.classList.add("symbols");
+    }
+
+    //Lights
+    if (settings.theme === "lights") {
+      cell.classList.add("lights");
+    }
+  }
+}
+
+//Handle Cell Click
+function handleCellClick(event) {
+  if (!gameState.gameActive) return;
+
+  const cell = event.target;
+  const index = parseInt(cell.dataset.index);
+
+  //Strict Mode
+  if (settings.strictMode && !gameState.pattern.includes(index)) {
+    gameState.lives--;
+
+    messageArea.textContent = "Wrong title! (Strict Mode)";
+    highlightCells("wrong");
+
+    updateDisplay();
+    addLog("Strict mode: wrong click (-1 life");
+
+    if (gameState.lives <= 0) {
+      gameOver();
+    }
+  }
+
+  //Toggle selection
+  if (cell.classList.contains("selected")) {
+    cell.classList.remove("selected");
+    gameState.userPattern = gameState.userPattern.filter((i) => i !== index);
+  } else {
+    cell.classList.add("selected");
+    gameState.userPattern.push(index);
+  }
+  console.log("User pattern:", gameState.userPattern);
+}
+
+createGrid();
+
+// Generate Pattern
+function generatePattern() {
+  gameState.pattern = [];
+
+  const totalCells = gameState.gridSize * gameState.gridSize;
+
+  //Pattern length increases with round
+  let length;
+
+  if (settings.difficulty === "easy") {
+    length = Math.max(2, gameState.round + 1);
+  } else if (settings.difficulty === "medium") {
+    length = Math.max(3, gameState.round + 2);
+  } else {
+    length = Math.max(4, gameState.round + 3);
+  }
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * totalCells);
+
+    //Avoid duplicates
+    if (!gameState.pattern.includes(randomIndex)) {
+      gameState.pattern.push(randomIndex);
+    } else {
+      i--; //retry
+    }
+  }
+  console.log("Pattern:", gameState.pattern);
+}
+
+//Show Pattern
+function showPattern() {
+  Array.from(gridArea.children).forEach((cell) => {
+    cell.classList.remove("hint");
+  });
+
+  let timeLeft = gameState.previewTime;
+
+  gameState.pattern.forEach((index) => {
+    const cell = gridArea.children[index];
+
+    //Base highlight
+    cell.classList.add("preview");
+
+    //Lights
+    if (settings.theme === "lights") {
+      cell.style.boxShadow = "0 0 15px yellow";
+      cell.style.backgroundColor = "#444";
+    }
+  });
+
+  //Timer Display
+  if (settings.showPreviewTime) {
+    timerBar.style.width = "100%";
+
+    const interval = setInterval(() => {
+      timeLeft -= 100;
+
+      const percentage = (timeLeft / TotalTime) * 100;
+      timerBar.style.width = percentage + "%";
+
+      if (timeLeft <= 0) {
+        clearInterval(interval);
+      }
+    }, 100);
+
+    messageArea.textContent = "Memorise the pattern...";
+  } else {
+    timerBar.style.width = "0%";
+    messageArea.textContent = "Memorise the pattern...";
+  }
+
+  //Hide after preview time
+  setTimeout(hidePattern, gameState.previewTime);
+}
+
+//Hide Pattern
+function hidePattern() {
+  timerBar.style.width = "0%";
+
+  Array.from(gridArea.children).forEach((cell) => {
+    cell.classList.remove("preview");
+
+    if (settings.theme === "lights") {
+      cell.style.boxShadow = "none";
+      cell.style.backgroundColor = "";
+    }
+  });
+
+  if (settings.showHints) {
+    gameState.pattern.forEach((index) => {
+      const cell = gridArea.children[index];
+      cell.classList.add("hint");
+    });
+  }
+
+  messageArea.textContent = "Now copy the pattern!";
+
+  gameState.gameActive = true;
+}
+
+//Start Game
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+  gameState.round = 1;
+  gameState.score = 0;
+  gameState.lives = 3;
+  gameState.userPattern = [];
+
+  gameState.previewTime = getPreviewTime();
+
+  gameState.gameActive = false;
+
+  gridArea.innerHTML = "";
+  createGrid();
+
+  setGameButtonsDisabled(false);
+
+  clearSelections();
+
+  updateDisplay();
+  generatePattern();
+  showPattern();
+
+  setTimeout(() => {
+    document.getElementById("gridArea").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  }, 200);
+
+  addLog("Game started");
+}
+
+function clearSelections() {
+  Array.from(gridArea.children).forEach((cell) => {
+    cell.classList.remove("selected", "correct", "wrong", "hint");
+  });
+}
+
+//Check Pattern Button
+const checkBtn = document.getElementById("checkBtn");
+
+checkBtn.addEventListener("click", checkPattern);
+
+//Compare the Patterns
+function checkPattern() {
+  if (!gameState.gameActive) return;
+
+  gameState.gameActive = false;
+
+  const correct = arraysMatch(gameState.pattern, gameState.userPattern);
+
+  if (correct) {
+    handleCorrect();
+  } else {
+    handleWrong();
+  }
+}
+
+function arraysMatch(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  //Sort both arrays before comparing
+  const sorted1 = [...arr1].sort();
+  const sorted2 = [...arr2].sort();
+
+  return sorted1.every((val, index) => val === sorted2[index]);
+}
+
+const resultsArea = document.getElementById("resultsArea");
+
+//Correct Answer
+function handleCorrect() {
+  let multiplier = 1;
+
+  //Difficulty
+  if (settings.difficulty === "easy") multiplier = 1;
+  if (settings.difficulty === "medium") multiplier = 1.5;
+  if (settings.difficulty === "hard") multiplier = 2;
+
+  //Hint penalty
+  if (settings.showHints) {
+    multiplier *= 0.7; //reduce score;
+  }
+
+  //Strict bonus
+  if (settings.strictMode) {
+    multiplier *= 1.5; //Increase score
+  }
+
+  const points = Math.round(gameState.round * 10 * multiplier);
+
+  gameState.score += points;
+
+  let note = "";
+  if (settings.showHints) note += " (Hints penalty)";
+  if (settings.strictMode) note += " (Strict bonus)";
+
+  messageArea.textContent = "Correct!";
+  highlightCells("correct");
+
+  updateDisplay();
+
+  resultsArea.textContent =
+    `Round ${gameState.round}\n` +
+    `Results: Correct\n` +
+    `Points: +${points}\n` +
+    `Total Score: ${gameState.score}`;
+
+  addLog(`Round ${gameState.round}: Correct (+${points})`);
+}
+
+//Wrong Answer
+function handleWrong() {
+  if (gameState.lives <= 0) return;
+
+  messageArea.textContent = "Wrong pattern!";
+
+  gameState.lives--;
+
+  highlightCells("wrong");
+
+  updateDisplay();
+
+  resultsArea.textContent = `Round ${gameState.round}: Incorrect!`;
+
+  addLog(`Round ${gameState.round}: Wrong (-1 life)`);
+
+  if (gameState.lives <= 0) {
+    addLog(`Lives remaining: ${gameState.lives}`);
+    gameOver();
+  }
+}
+
+//Highlight Cells
+function highlightCells(type) {
+  gameState.pattern.forEach((index) => {
+    const cell = gridArea.children[index];
+    cell.classList.add(type);
+  });
+}
+
+//Game Over
+function gameOver() {
+  gameState.gameActive = false;
+
+  setGameButtonsDisabled(true);
+
+  alert("Game Over!");
+
+  if (gameState.score > player.bestScore) {
+    player.bestScore = gameState.score;
+    document.cookie = `bestScore=${player.bestScore}`;
+  }
+
+  addLog(`Game over - Score: ${gameState.score}`);
+
+  resetGame();
+}
+
+//Reset Game
+const resetBtn = document.getElementById("resetBtn");
+
+resetBtn.addEventListener("click", () => {
+  if (confirm("Reset game?")) {
+    resetGame();
+  }
+});
+
+function resetGame() {
+  gameState.round = 0;
+  gameState.score = 0;
+  gameState.lives = 3;
+  gameState.pattern = [];
+  gameState.userPattern = [];
+
+  clearSelections();
+  updateDisplay();
+
+  messageArea.textContent = "Game reset. Click Start Game.";
+}
+
+//Next Round
+const nextRoundBtn = document.getElementById("nextRoundBtn");
+
+nextRoundBtn.addEventListener("click", nextRound);
+
+function nextRound() {
+  if (gameState.lives <= 0) return;
+
+  gameState.round++;
+  gameState.userPattern = [];
+  gameState.gameActive = false;
+  gameState.previewTime = getPreviewTime();
+
+  clearSelections();
+
+  generatePattern();
+  showPattern();
+
+  setTimeout(() => {
+    document.getElementById("gridArea").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  }, 200);
+
+  updateDisplay();
+
+  addLog(`Round ${gameState.round} started`);
+}
+
+//Save Session
+const saveBtn = document.getElementById("saveBtn");
+
+saveBtn.addEventListener("click", saveSession);
+
+function saveSession() {
+  const saveData = {
+    gameState,
+    player,
+  };
+
+  sessionStorage.setItem("gameSave", JSON.stringify(saveData));
+
+  document.cookie = `playerName${player.name}`;
+  document.cookie = `bestScore=${player.bestScore}`;
+
+  addLog("Game saved");
+  alert("Game saved!");
+}
+
+//Load Session
+function loadSession() {
+  const data = sessionStorage.getItem("gameSave");
+
+  if (!data) {
+    alert("No saved session found!");
+    return;
+  }
+
+  const parsed = JSON.parse(data);
+
+  Object.assign(gameState, parsed.gameState);
+  Object.assign(player, parsed.player);
+
+  gameState.userPattern = [];
+  gameState.gameActive = false;
+
+  gridArea.innerHTML = "";
+  createGrid();
+
+  updateDisplay();
+
+  messageArea.textContent = "Session loaded. Ready to continue.";
+  resultsArea.textContent = "";
+
+  clearSelections();
+
+  addLog(`Session loaded (Round ${gameState.round})`);
+
+  alert("Session loaded!");
+}
+loadBtn.addEventListener("click", () => {
+  console.log("LOAD BUTTON CLICKED");
+  loadSession();
+});
+
+//Back Button
+const backBtn = document.getElementById("backBtn");
+
+backBtn.addEventListener("click", () => {
+  window.close();
+});
+
+//Game Logs
+const logArea = document.getElementById("logArea");
+
+function addLog(message) {
+  const entry = document.createElement("div");
+  entry.classList.add("log-entry");
+
+  const time = new Date().toLocaleTimeString();
+
+  entry.textContent = `[${time}] ${message}`;
+
+  logArea.prepend(entry);
+
+  console.log("LOG:", message);
+}
+
+function setGameButtonsDisabled(state) {
+  checkBtn.disabled = state;
+  nextRoundBtn.disabled = state;
+}
---
+++ launcher.js
@@ -0,0 +1,122 @@
+// DOM elements
+
+const playerNameInput = document.getElementById("playerName");
+const difficultySelect = document.getElementById("difficulty");
+const gridSizeSelect = document.getElementById("gridSize");
+const previewText = document.getElementById("previewText");
+
+const openGameBtn = document.getElementById("openGameBtn");
+const saveSettingsBtn = document.getElementById("saveSettingsBtn");
+const loadSettingsBtn = document.getElementById("loadSettingsBtn");
+const resetSettingsBtn = document.getElementById("resetSettingsBtn");
+const instructionsBtn = document.getElementById("instructionsBtn");
+
+//Settings object
+
+let settings = {
+    playerName: "",
+    difficulty: "Medium",
+    gridSize: 4,
+    theme: "Colours",
+    showHints: false,
+    strictMode: false,
+    showPreviewTime: true,
+};
+
+// Update settings from form
+
+function updateSettings(){
+    settings.playerName = playerNameInput.value.trim();
+    settings.difficulty = difficultySelect.value;
+    settings.gridSize = parseInt(gridSizeSelect.value);
+
+    settings.theme = document.querySelector("input[name='theme']:checked").value;
+
+    settings.showHints = document.getElementById("showHints").checked;
+    settings.strictMode = document.getElementById("strictMode").checked;
+    settings.showPreviewTime = document.getElementById("showPreviewTime").checked;
+
+    updatePreview();
+}
+
+// Live Preview
+
+function updatePreview() {
+    previewText.textContent = `
+Player: ${settings.playerName || "Not set"}
+Difficulty: ${settings.difficulty}
+Grid: ${settings.gridSize}x${settings.gridSize}
+Theme: ${settings.theme}
+Options: ${settings.strictMode ? "Strict " : ""}${settings.showHints ? "Hints " : ""}
+`;
+}
+
+// Save Settings (Cookies)
+
+function saveSettings() {
+    document.cookie = `playerName=${settings.playerName}`;
+    document.cookie = `bestScore=0`; //default for now
+    alert("Settings saved!");
+}
+
+//Load settings
+
+function loadSettings() {
+    const cookies = document.cookie.split(";");
+
+    cookies.forEach(c => {
+        const [key, value] = c.trim().split("=");
+        if (key === "playerName") {
+            playerNameInput.value = value;
+        }
+    });
+}
+
+updateSettings();
+console.log("Settings loaded!");
+
+// Reset Settings
+
+function resetSettings() {
+    if (confirm("Are you sure you want to reset settings?")) {
+        document.getElementById("setupForm").reset();
+        updateSettings();
+    }
+}
+
+// Open game Window
+
+function openGame() {
+    updateSettings();
+
+    if (!settings.playerName) {
+        settings.playerName = prompt("Enter your name:");
+        if (!settings.playerName) return;
+    }
+
+    //save settings in session storage
+    sessionStorage.setItem("settings", JSON.stringify(settings));
+
+    window.open("game.html", "_blank", "width=800,height=700");
+}
+
+//Navigation
+
+function openInstructions() {
+    window.location.href = "instructions.html";
+}
+
+// event listeners
+
+document.querySelectorAll("input, select").forEach( el => {
+    el.addEventListener("change", updateSettings);
+})
+
+openGameBtn.addEventListener("click", openGame);
+saveSettingsBtn.addEventListener("click", saveSettings);
+loadSettingsBtn.addEventListener("click", loadSettings);
+resetSettingsBtn.addEventListener("click", resetSettings);
+instructionsBtn.addEventListener("click", openInstructions);
+
+// Init preview
+updateSettings();
